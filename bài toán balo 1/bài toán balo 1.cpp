1d021bc80be09e360cc511cad49ad999c434253e
Nguyễn Thị Hồng Phượng <133510497+NguyenThiHongPhuong-1705-1102-2002-237@users.noreply.github.com>
2023-05-17 17:53:52 +0700

    Add files via upload

---
+++ bài toán balo 1/bài toán balo 1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int maketqua[100], gtkhoiluong[100], MAX = 0, khoiluongmax, somonhang, n;
+string codekq = "", codetam = "";
+
+void sauKhiDeQui() {
+	int khoiluong = 0;
+	codetam = "";
+	for (int i = 0; i < somonhang; ++i) {
+		khoiluong += maketqua[i] * gtkhoiluong[i];
+	}
+	for (int i = 0; i < somonhang; ++i) {
+		if (maketqua[i] != 0) {
+			string s;
+			s = '0' + i;
+			codetam += s + " ";
+
+		}
+	}
+	if (khoiluong <= khoiluongmax && khoiluong > MAX) {
+		MAX = khoiluong;
+		codekq = codetam;
+	}
+}
+
+void deQui(int i) {
+	if (i == somonhang)
+		sauKhiDeQui();
+	else {
+		for (int j = 0; j <= 1; ++j) {
+			maketqua[i] = j;
+			deQui(i + 1);
+		}
+	}
+}
+
+int main() {
+	ifstream infile;
+	infile.open("balo.inp");
+	infile >> somonhang >> khoiluongmax;
+	for (int i = 0; i < somonhang; ++i)
+		infile >> gtkhoiluong[i];
+	infile.close();
+	ofstream outfile;
+	outfile.open("balo.out");
+	deQui(0);
+	outfile << MAX << endl << codekq;
+	outfile.close();
+	return 0;
+}
---
+++ bài toán balo 2/balo 2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int* aWeight, * aValue, * b, * maxBool;
+int weight, limitWeight, maxValue, value;
+int n;
+void Try(int j) {
+    if (j == n) {
+        weight = value = 0;
+        for (int i = 0; i < n; i++) {
+            if (b[i]) {
+                weight += aWeight[i];
+                value += aValue[i];
+            }
+        }
+        if (weight < limitWeight) {
+            if (value > maxValue) {
+                maxValue = value;
+                for (int i = 0; i < n; i++) {
+                    maxBool[i] = b[i];
+                }
+            }
+        }
+
+    }
+    else {
+        b[j] = 0;
+        Try(j + 1);
+        b[j] = 1;
+        Try(j + 1);
+    }
+}
+
+int main() {
+    cin >> n >> limitWeight;
+    aWeight = new int[n];
+    aValue = new int[n];
+    b = new int[n];
+    maxBool = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> aWeight[i] >> aValue[i];
+    }
+
+    Try(0);
+
+    cout << maxValue << endl;
+    for (int i = 0; i < n; i++) {
+        cout << maxBool[i] << ' ';
+    }
+}
